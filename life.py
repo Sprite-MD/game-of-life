c4eca11293c003432b3dffdf001dc81a53b4c233
Sprite <ponkaew.wasin@gmail.com>
2022-07-19 17:34:49 -0500

    finished logic core

--- life.py
+++ life.py
@@ -16,4 +16,19 @@ def next(grid, rows, cols):
             # current cell must be subtracted since ^^ added it to the counter
             liveNeighbors -= grid[row][col]
 
-            
+            # Rule One: Lonely cell dies
+            if grid[row][col] == 1 and liveNeighbors < 2:
+                future[row][col] == 0
+
+            # Rule Two: Overpopulated cell dies
+            elif grid[row][col] == 1 and liveNeighbors > 3:
+                future[row][col] == 0
+
+            # Rule Three: New cell is born
+            elif grid[row][col] == 0 and liveNeighbors == 3:
+                future[row][col] == 1
+
+            # Nothing changes
+            else:
+                future[row][col] == grid[row][col]
+
